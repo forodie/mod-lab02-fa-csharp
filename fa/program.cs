1ec1fa7cc4f1ef27bbd20f6d60750867fa61b830
German (forodie) <german.utkingu@gmail.com>
2023-03-20 16:45:55 +0300

    fixed fa1

--- fa/program.cs
+++ fa/program.cs
@@ -44,16 +44,27 @@ namespace fans
             Transitions = new Dictionary<char, State>()
         };
 
+        public static State q4 = new State()
+        {
+            Name = "q4",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
         State InitialState = q0;
 
         public FA1()
         {
             q0.Transitions['0'] = q1;
             q0.Transitions['1'] = q3;
+            q1.Transitions['0'] = q4;
             q1.Transitions['1'] = q2;
+            q2.Transitions['0'] = q4;
             q2.Transitions['1'] = q2;
             q3.Transitions['0'] = q2;
             q3.Transitions['1'] = q0;
+            q4.Transitions['0'] = q4;
+            q4.Transitions['1'] = q4;
         }
 
         public bool? Run(IEnumerable<char> s)
